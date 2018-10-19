9bc7ae5d2d1452ad2e1dab8fc5379bbcb8b669af
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2018-10-19 10:51:11 +0200

    Inventory Manager added

---
+++ Assets/Scripts/CM/Essentials/Inventory/Inventory.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace CM.Essentials.Inventory
+{
+	[System.Serializable]
+	public class Inventory : IInventory
+	{
+		[SerializeField] private string _title;
+		public string Title { get { return _title; } set { _title = value; } }
+
+		private Item[,] _stock;
+
+		public Inventory(string title, int rows, int colomns)
+		{
+			_title = title;
+			_stock = new Item[rows, colomns];
+		}
+
+		public Item GetItem(int row, int colomn)
+		{
+			return _stock[row, colomn];
+		}
+
+		public void AddItem(Item item, int row, int colomn)
+		{
+			_stock[row, colomn] = item;
+		}
+	}
+}
---
+++ Assets/Scripts/CM/Essentials/Inventory/InventoryManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CM.Essentials.Inventory
+{
+	public class InventoryManager : MonoBehaviour
+	{
+		[SerializeField] private List<Inventory> _inventories = new List<Inventory>();
+		[SerializeField] private List<Item> _items = new List<Item>();
+	}
+}
